e1f9bb8a27b254d8025b0e9073e35e71a035304b
bug3 <bug3dev@gmail.com>
2022-02-26 23:18:09 +0300

    Add wget command

--- install.sh
+++ install.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+exists () {
+    command -v $1 > /dev/null 2>&1
+}
+
 if [[ $# -eq 2 || $# -eq 3 ]]; then
     user=$1
     repo=$2
     [[ $# -eq 3 ]] && branch=$3 || branch="master"
-
-	curl -JLO https://github.com/$user/$repo/archive/$branch.zip
+    archiveUrl="https://github.com/$user/$repo/archive/$branch.zip"
     
-    unzip $repo-$branch.zip
-    cd $repo-$branch
+    if exists curl; then
+        curl -JLO $archiveUrl
+        unzip $repo-$branch.zip
+    elif exists wget; then
+        wget $archiveUrl
+        unzip $branch.zip
+    else
+        echo "curl or wget command not found"
+        exit 127
+    fi
 
+    cd $repo-$branch
     /bin/bash setup.sh
 else
 	echo "args error"
