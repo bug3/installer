e31287f29d1ef4bf6f86acae77da374a0cb6c54d
bug3 <bug3dev@gmail.com>
2022-02-28 02:46:59 +0300

    Edit unzip usage

--- install.sh
+++ install.sh
@@ -23,27 +23,29 @@ if [[ $# -eq 2 || $# -eq 3 ]]; then
     [[ $# -eq 3 ]] && branch=$3 || branch="master"
     archiveUrl="https://github.com/$user/$repo/archive/$branch.zip"
     reposUrl="https://api.github.com/repos/$user/$repo"
+    dirname=$repo-$branch
 
     if exists curl; then
         curlResponse=$(wget $reposUrl -SO /dev/null 2>&1 | awk '/^  HTTP/{printf $2}')
 
         if checkRepo $curlResponse; then
             curl -JLO $archiveUrl
-            unzip $repo-$branch.zip
         fi
     elif exists wget; then
         wgetResponse=$(curl -so /dev/null -w '%{http_code}' $reposUrl)
 
         if checkRepo $wgetResponse; then
             wget $archiveUrl
-            unzip $branch.zip
+            dirname=$branch
         fi
     else
         exitWithError "curl or wget command not found"
     fi
 
-    rm $repo-$branch.zip
+    unzip $dirname.zip
+    rm $dirname.zip
     cd $repo-$branch
+
     /bin/bash setup.sh
 else
 	exitWithError "Wrong number of args"
