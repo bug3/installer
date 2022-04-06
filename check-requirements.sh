137e679027307767f2cd6012a618a898d5b18191
bug3 <bug3dev@gmail.com>
2022-04-07 02:19:49 +0300

    Install linux packages

--- check-requirements.sh
+++ check-requirements.sh
@@ -3,3 +3,5 @@
 file="requirements.json"
 url="https://raw.githubusercontent.com/bug3/installer/master/getpm.sh"
 pm=$(bash <(curl -sL $url))
+
+sudo $pm install -y $(jq -r ['.linux[]','keys[1:][]'][] $file)
