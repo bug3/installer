dd8bd9f0f4b141447e02e803f92bfc8e714f0816
bug3 <bug3dev@gmail.com>
2022-04-07 02:21:58 +0300

    Install defined packages

--- check-requirements.sh
+++ check-requirements.sh
@@ -5,3 +5,24 @@ url="https://raw.githubusercontent.com/bug3/installer/master/getpm.sh"
 pm=$(bash <(curl -sL $url))
 
 sudo $pm install -y $(jq -r ['.linux[]','keys[1:][]'][] $file)
+
+for p in $(jq -r keys[1:][] $file); do
+    values="jq -r '.\"$p\"[]' requirements.json"
+
+    case $p in
+        npm)
+            sudo npm install -g $(eval $values)
+            ;;
+        snapd)
+            sudo snap install $(eval $values)
+            ;;
+        python3-pip)
+            pip install $(eval $values)
+            ;;
+        *)
+            $p install $(eval $values)
+            ;;
+    esac
+
+    echo ""
+done
