9c2b40095afac94d2b05be55213c62d00590114d
bug3 <bug3dev@gmail.com>
2022-04-07 00:16:05 +0300

    Select pm

--- getpm.sh
+++ getpm.sh
@@ -7,3 +7,9 @@ osInfo["/etc/centos-release"]=yum
 osInfo["/etc/SuSE-release"]=zypp
 osInfo["/etc/fedora-release"]=dnf
 osInfo["/etc/arch-release"]=pacman
+
+for f in ${!osInfo[@]}; do
+    if [[ -f $f ]]; then
+        echo ${osInfo[$f]}
+    fi
+done
