6c2923921dcc86c7c957a2f45e26be212176cd2d
bug3 <bug3dev@gmail.com>
2022-04-07 00:15:41 +0300

    Declare osInfo array

--- getpm.sh
+++ getpm.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+declare -A osInfo
+
+osInfo["/etc/debian_version"]=apt
+osInfo["/etc/centos-release"]=yum
+osInfo["/etc/SuSE-release"]=zypp
+osInfo["/etc/fedora-release"]=dnf
+osInfo["/etc/arch-release"]=pacman
