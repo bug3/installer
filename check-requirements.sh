42fdeb4fe6f60ea3b3004dc45034134435f0c815
bug3 <bug3dev@gmail.com>
2022-04-07 22:54:53 +0300

    Define checkInstall func

--- check-requirements.sh
+++ check-requirements.sh
@@ -4,25 +4,27 @@ file="requirements.json"
 url="https://raw.githubusercontent.com/bug3/installer/master/getpm.sh"
 pm=$(bash <(curl -sL $url))
 
-sudo $pm install -y $(jq -r ['.linux[]','keys[1:][]'][] $file)
+checkInstall() {
+    sudo $pm install -y $(jq -r ['.linux[]','keys[1:][]'][] $file)
 
-for p in $(jq -r keys[1:][] $file); do
-    values="jq -r '.\"$p\"[]' requirements.json"
+    for p in $(jq -r keys[1:][] $file); do
+        values="jq -r '.\"$p\"[]' requirements.json"
 
-    case $p in
-        npm)
-            sudo npm install -g $(eval $values)
-            ;;
-        snapd)
-            sudo snap install $(eval $values)
-            ;;
-        python3-pip)
-            pip install $(eval $values)
-            ;;
-        *)
-            $p install $(eval $values)
-            ;;
-    esac
+        case $p in
+            npm)
+                sudo npm install -g $(eval $values)
+                ;;
+            snapd)
+                sudo snap install $(eval $values)
+                ;;
+            python3-pip)
+                pip install $(eval $values)
+                ;;
+            *)
+                $p install $(eval $values)
+                ;;
+        esac
 
-    echo ""
-done
+        echo ""
+    done
+}
