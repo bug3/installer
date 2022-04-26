6ad338b3687a8f2272e80580c941b72f4028f28b
bug3 <bug3dev@gmail.com>
2022-04-26 16:51:48 +0300

    Define params

--- install.ps1
+++ install.ps1
@@ -1,6 +1,11 @@
 New-Module -name Installer -scriptblock {
     function install() {
-
+        param (
+            [Parameter(Mandatory = $true)] $user,
+            [Parameter(Mandatory = $true)] $repo,
+            [string]$branch = "master",
+            [bool]$remove = $false
+        )
     }
     
     Export-ModuleMember -function "install"
