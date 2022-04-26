4b3959be3e85800402275e29d37b82608b46539c
bug3 <bug3dev@gmail.com>
2022-04-26 16:52:17 +0300

    Define vars

--- install.ps1
+++ install.ps1
@@ -6,6 +6,12 @@ New-Module -name Installer -scriptblock {
             [string]$branch = "master",
             [bool]$remove = $false
         )
+
+        $zipFile = "$repo.zip"
+        $project = "$repo-$branch"
+        $reposUrl = "https://api.github.com/repos/$user/$repo"
+        $archiveUrl = "https://github.com/$user/$repo/archive/$branch.zip"
+        $statusCode = Invoke-WebRequest $reposUrl | Select-Object -Expand StatusCode
     }
     
     Export-ModuleMember -function "install"
